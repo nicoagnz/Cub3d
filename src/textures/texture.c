43c3c7a7ceaf4d965be0237291fc013c536c8853
ctaboada <ctaboada@student.42malaga.com>
2026-03-11 12:01:39 +0100

    ya esta perfecto compi

--- src/textures/texture.c
+++ src/textures/texture.c
@@ -35,30 +35,37 @@ static int	calc_tex_x(t_game *game, t_dda *dda, mlx_texture_t *tex,
 		wall_x = game->player.x + perp_dist * ray_dir[0];
 	wall_x -= floor(wall_x);
 	tex_x = (int)(wall_x * (double)tex->width);
-	if ((dda->side == 0 && ray_dir[0] > 0) || (dda->side == 1 && ray_dir[1] < 0))
+	if (dda->side == 0 && ray_dir[0] > 0)
 		tex_x = tex->width - tex_x - 1;
+	if (dda->side == 1 && ray_dir[1] < 0)
+		tex_x = tex->width - tex_x - 1;
+	if (tex_x < 0)
+		tex_x = 0;
+	if (tex_x >= (int)tex->width)
+		tex_x = tex->width - 1;
 	return (tex_x);
 }
 
 static void	draw_column_texture(t_game *game, int x, mlx_texture_t *tex,
-				int data[3], double perp_dist, int side)
+				int data[3], int line_height)
 {
 	double		step;
 	double		tex_pos;
 	int			y;
-	int			line_height;
 	uint32_t	color;
+	int			tex_y;
 
-	line_height = (int)(game->win_h / perp_dist);
 	step = 1.0 * tex->height / line_height;
 	tex_pos = (data[0] - game->win_h / 2 + line_height / 2) * step;
 	y = data[0];
 	while (y <= data[1])
 	{
-		color = get_texture_pixel(tex, data[2], (int)tex_pos);
-		// Oscurecer los lados Y para dar efecto de sombra
-		if (side == 1)
-			color = (color >> 1) & 0x7F7F7F7F;
+		tex_y = (int)tex_pos;
+		if (tex_y < 0)
+			tex_y = 0;
+		else if (tex_y >= (int)tex->height)
+			tex_y = tex->height - 1;
+		color = get_texture_pixel(tex, data[2], tex_y);
 		mlx_put_pixel(game->frame.img, x, y, color);
 		tex_pos += step;
 		y++;
@@ -70,10 +77,12 @@ void	draw_texture(t_game *game, int x, t_dda *dda, int limits[2],
 {
 	mlx_texture_t	*tex;
 	int				data[3];
+	int				line_height;
 
 	tex = get_texture(game, dda);
+	line_height = (int)(game->win_h / perp_dist);
 	data[0] = limits[0];
 	data[1] = limits[1];
 	data[2] = calc_tex_x(game, dda, tex, ray_dir, perp_dist);
-	draw_column_texture(game, x, tex, data, perp_dist, dda->side);
+	draw_column_texture(game, x, tex, data, line_height);
 }
